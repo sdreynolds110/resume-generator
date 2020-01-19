50c3620e1dec6882f01d6e6a6e23a4096f4485bf
Stephen Reynolds <sdreynolds110@gmail.com>
2020-01-18 23:30:13 -0500

    added html file and edited index js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
-const generateHTML = require("./generatedHTML");
+const generateHTML = require("./generateHTML");
 
 
 gitHubInfo = async (username, chosenColor) => {
