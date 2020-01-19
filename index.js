356c2462ecb8ae7b870dea905bb531d8164996c0
Stephen Reynolds <sdreynolds110@gmail.com>
2020-01-18 21:48:58 -0500

    began homework

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const generateHTML = require("./generatedHTML");
+
+
+gitHubInfo = async (username, chosenColor) => {
+  let res = await axios.get("https://api.github.com/users/" + username);
+  const data = res.data;
+  await fs.writeFile(
+
+    generateHTML.generateHTML(data, chosenColor),
+    function(err) {
+      if (err) {
+        return console.log(err);
+      } else {
+        console.log("finished");
+      }
+    }
+  );
+
+  await conversion(
+    { html: generateHTML.generateHTML(data, chosenColor) },
+    function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      result.stream.pipe(fs.createWriteStream("./index.pdf"));
+      conversion.kill();
+    }
+  );
+};
+
+
+function getUserInput() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter a GitHub username:",
+        name: "username"
+      },
+      {
+        type: "input",
+        message: "Enter a color:",
+        name: "color"
+      }
+    ])
+
+    .then(answers => {
+      const username = answers.username;
+      const chosenColor = answers.color;
+      gitHubInfo(username, chosenColor);
+    });
+}
+
+getUserInput();
